748689b7ee650ee07b642e1208d982b9c28f6461
psarge2 <54369285+psarge2@users.noreply.github.com>
2021-03-10 09:44:06 -0500

    updated header and footer .php files

--- footer.php
+++ footer.php
@@ -1,4 +1,12 @@
-    <!-- Bootstrap Javascript -->          
+      <footer class="footer text-center py-2 theme-bg-dark">
+		   
+            <p class="copyright"><a href="https://sargesites.com">SargeSites</a></p>
+		   
+	    </footer>
+    
+    </div>
+
+<!-- Bootstrap Javascript -->          
     <?php
       wp_footer();
     ?>
